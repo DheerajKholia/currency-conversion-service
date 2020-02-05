50634b62a29a8cad7618ad12666fd4566c57b13f
DheerajKholia <dheraj.kholia@yahoo.com>
2020-02-05 20:56:51 +0530

    Using Feign client to invoke rest API

--- src/main/java/com/dheeraj/midroservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/com/dheeraj/midroservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,8 +2,10 @@ package com.dheeraj.midroservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.dheeraj.midroservices.currencyconversionservice")
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
